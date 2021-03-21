26f397e8b2d21bd3dee593a3203a7eec2b9d1b49
Neosvet <neosvet333@gmail.com>
2021-03-21 11:18:43 +0300

    created data class

---
+++ app/src/main/java/ru/neosvet/moviedb/Movie.kt
@@ -0,0 +1,5 @@
+package ru.neosvet.moviedb
+
+data class Movie(val title: String, val description: String, val genres: String, val year: Int,
+                 val country: String, val poster: String) {
+}
