b9df5761759c3626b771deb49bc70ff0b21475c1
Neosvet <neosvet333@gmail.com>
2021-03-21 11:46:48 +0300

    added clickListener

--- app/src/main/java/ru/neosvet/moviedb/MainActivity.kt
+++ app/src/main/java/ru/neosvet/moviedb/MainActivity.kt
@@ -2,10 +2,26 @@ package ru.neosvet.moviedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etSearch: TextInputEditText
+    private lateinit var btnSearch: MaterialButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews();
+    }
+
+    private fun initViews() {
+        etSearch = findViewById(R.id.etSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener {
+            Toast.makeText(this, etSearch.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
